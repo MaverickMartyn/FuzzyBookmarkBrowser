9f297ecbb281640937475d65df5212bab23b5b06
Martin Finnerup <contact@codeslasher.com>
2021-11-08 03:10:26 +0100

    Add project files.

---
+++ FuzzyBookmarkBrowser/Bookmark.cs
@@ -0,0 +1,181 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace FuzzyBookmarkBrowser
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+    public class Child
+    {
+        [JsonProperty("date_added")]
+        public string DateAdded { get; set; }
+
+        [JsonProperty("guid")]
+        public string Guid { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        private string _name;
+
+        [JsonProperty("name")]
+        public string Name
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(_name))
+                {
+                    _name = "[N/A]";
+                }
+                return _name;
+            }
+            set {  _name = value; }
+        }
+
+        [JsonIgnore]
+        public string TypeImg { get { return Type == "folder" ? "./folder.png" : "./file.png"; } }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("meta_info")]
+        public MetaInfo MetaInfo { get; set; }
+        
+        [JsonIgnore]
+        public int Likeness { get; set; }
+
+        [JsonProperty("children")]
+        public List<Child> Children { get; set; }
+
+        public override string ToString()
+        {
+            return $"{ Name } ({ Url }) - { Type } - { MetaInfo?.LastVisited }";
+        }
+
+        public System.Windows.Media.Brush SearchBg
+        {
+            get
+            {
+                return Likeness < 1 ? System.Windows.Media.Brushes.LightSlateGray : System.Windows.Media.Brushes.Transparent;
+            }
+        }
+
+        [JsonIgnore]
+        public bool Hidden { get; set; }
+    }
+
+    public class BookmarkBar
+    {
+        [JsonProperty("children")]
+        public List<Child> Children { get; set; }
+
+        [JsonProperty("date_added")]
+        public string DateAdded { get; set; }
+
+        [JsonProperty("date_modified")]
+        public string DateModified { get; set; }
+
+        [JsonProperty("guid")]
+        public string Guid { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+    }
+
+    public class MetaInfo
+    {
+        [JsonProperty("last_visited_desktop")]
+        public string LastVisitedDesktop { get; set; }
+
+        [JsonProperty("last_visited")]
+        public string LastVisited { get; set; }
+    }
+
+    public class Other
+    {
+        [JsonProperty("children")]
+        public List<Child> Children { get; set; }
+
+        [JsonProperty("date_added")]
+        public string DateAdded { get; set; }
+
+        [JsonProperty("date_modified")]
+        public string DateModified { get; set; }
+
+        [JsonProperty("guid")]
+        public string Guid { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+    }
+
+    public class Synced
+    {
+        [JsonProperty("children")]
+        public List<Child> Children { get; set; }
+
+        [JsonProperty("date_added")]
+        public string DateAdded { get; set; }
+
+        [JsonProperty("date_modified")]
+        public string DateModified { get; set; }
+
+        [JsonProperty("guid")]
+        public string Guid { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+    }
+
+    public class Roots
+    {
+        [JsonProperty("bookmark_bar")]
+        public BookmarkBar BookmarkBar { get; set; }
+
+        [JsonProperty("other")]
+        public Other Other { get; set; }
+
+        [JsonProperty("synced")]
+        public Synced Synced { get; set; }
+    }
+
+    public class Root
+    {
+        [JsonProperty("checksum")]
+        public string Checksum { get; set; }
+
+        [JsonProperty("roots")]
+        public Roots Roots { get; set; }
+
+        [JsonProperty("sync_metadata")]
+        public string SyncMetadata { get; set; }
+
+        [JsonProperty("version")]
+        public int Version { get; set; }
+    }
+
+
+}
---
+++ FuzzyBookmarkBrowser/MainWindow.xaml.cs
@@ -0,0 +1,297 @@
+﻿using Microsoft.Win32;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Environment;
+
+namespace FuzzyBookmarkBrowser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        protected void HandleDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var bookmark = ((TreeViewItem)sender).DataContext as Child;
+            if (bookmark != null && bookmark.Type != "folder")
+            {
+                LaunchURL(bookmark.Url);
+            }
+        }
+
+        internal string GetSystemDefaultBrowser()
+        {
+            const string userChoice = @"Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice";
+            string progId;
+            using (RegistryKey userChoiceKey = Registry.CurrentUser.OpenSubKey(userChoice))
+            {
+                object progIdValue = userChoiceKey?.GetValue("Progid");
+                if (progIdValue == null)
+                {
+                    throw new InvalidOperationException();
+                }
+                progId = progIdValue.ToString();
+
+                const string exeSuffix = ".exe";
+                string path = progId + @"\shell\open\command";
+                //FileInfo browserPath;
+                using (RegistryKey pathKey = Registry.ClassesRoot.OpenSubKey(path))
+                {
+                    if (pathKey == null)
+                    {
+                        throw new InvalidOperationException();
+                    }
+
+                    // Trim parameters.
+                    try
+                    {
+                        path = pathKey.GetValue(null).ToString().ToLower().Replace("\"", "");
+                        if (!path.EndsWith(exeSuffix))
+                        {
+                            path = path.Substring(0, path.LastIndexOf(exeSuffix, StringComparison.Ordinal) + exeSuffix.Length);
+                            //browserPath = new FileInfo(path);
+                        }
+                    }
+                    catch
+                    {
+                        // Assume the registry value is set incorrectly, or some funky browser is used which currently is unknown.
+                        throw new InvalidOperationException();
+                    }
+                }
+                return path;
+            }
+
+
+        }
+
+        private void LaunchURL(string url)
+        {
+            string privateModeParam = string.Empty;
+            string browserName = GetSystemDefaultBrowser();
+            if (string.IsNullOrEmpty(browserName))
+            {
+                MessageBox.Show("No default browser found!");
+            }
+            else
+
+            {
+                if (cbx_openInIncognito.IsChecked.HasValue && cbx_openInIncognito.IsChecked.Value)
+                {
+                    if (browserName.Contains("firefox"))
+                    {
+                        privateModeParam = " -private-window";
+                    }
+                    else if ((browserName.Contains("iexplore")) || (browserName.Contains("Opera")))
+                    {
+                        privateModeParam = " -private";
+                    }
+                    else if (browserName.Contains("chrome"))
+                    {
+                        privateModeParam = " -incognito";
+                    }
+                }
+                Process.Start(browserName, $"{privateModeParam} {url}");
+            }
+        }
+
+        List<Child> bookmarks;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Task.Run(() =>
+            {
+                UpdateBookmarkList();
+                Application.Current.Dispatcher.Invoke(new Action(() => {
+                    tv_bookmarks.ItemsSource = bookmarks;
+                }));
+            });
+        }
+
+        private List<Child> SortBookmarks(List<Child> unorderedBookmarks)
+        {
+            List<Child> sort = new List<Child>();
+            Func<List<Child>, List<Child>> selector = null;
+            selector = (inputList) => {
+                var topLayer = inputList.OrderBy(x => x.Type).ThenByDescending(x => x.Likeness).ThenBy(x => x.Name).ToList();
+                foreach (var bm in topLayer)
+                {
+                    if (bm.Children != null)
+                        bm.Children = selector.Invoke(bm.Children);
+                }
+                return topLayer;
+            };
+
+            sort = selector.Invoke(unorderedBookmarks);
+
+            return sort;
+        }
+
+        //private List<Child> SortBookmarks(List<Child> unorderedBookmarks)
+        //{
+        //    List<Child> sort = new List<Child>();
+        //    Action<List<Child>, int> selector = null;
+        //    var lvl = 0;
+        //    selector = (inputList, curLvl) => {
+        //        var topLayer = inputList.OrderByDescending(x => x.Likeness).ThenBy(x => x.Name);
+        //        foreach (var bm in topLayer)
+        //        {
+        //            bm.Lvl = curLvl;
+        //            sort.Add(bm);
+        //            if (bm.Children != null)
+        //                selector.Invoke(bm.Children, curLvl + 1);
+        //        }
+        //    };
+
+        //    selector.Invoke(unorderedBookmarks, lvl);
+
+        //    return sort;
+        //}
+
+        private void UpdateBookmarkList()
+        {
+            var bookmarksJson = File.ReadAllText(System.IO.Path.GetFullPath(System.IO.Path.Combine(Environment.GetFolderPath(SpecialFolder.LocalApplicationData), @"Google\Chrome\User Data\Default\bookmarks")));
+            if (!string.IsNullOrWhiteSpace(bookmarksJson))
+            {
+                Root? bookmarksRoot = JsonConvert.DeserializeObject<Root>(bookmarksJson);
+                if (bookmarksJson != null)
+                {
+                    string srchTxt = "";
+                    Application.Current.Dispatcher.Invoke(new Action(() => {
+                        srchTxt = tb_search.Text;
+                    }));
+                    bookmarks = new List<Child>();
+                    bookmarks.AddRange(bookmarksRoot.Roots.BookmarkBar.Children);
+                    bookmarks.AddRange(bookmarksRoot.Roots.Other.Children);
+                    bookmarks.AddRange(bookmarksRoot.Roots.Synced.Children);
+                    bookmarks = RankBookmarks(srchTxt, bookmarks);
+                    bookmarks = SortBookmarks(bookmarks);
+                }
+
+                //lv_bookmarks.ItemsSource = bookmarks;
+                //callback();
+            }
+        }
+
+        DateTime _nextUpdateAllowed;
+        private Task _refreshQueueTask;
+        int _cntMatches = 0;
+
+        private List<Child> RankBookmarks(string srchTxt, List<Child> unorderedBookmarks)
+        {
+            bool onlyShowMatches = false;
+            Application.Current.Dispatcher.Invoke(new Action(() => {
+                onlyShowMatches = cbx_onlyShowMatches.IsChecked.HasValue && cbx_onlyShowMatches.IsChecked.Value;
+            }));
+
+            _cntMatches = 0;
+            char[] splitChars = new[] { ' ', '-', '/' };
+
+            string[] filters = srchTxt.Split(splitChars).Where(n => !String.IsNullOrWhiteSpace(n)).ToArray();
+            List<Child> sort = new List<Child>();
+            Func<List<Child>, List<Child>> selector = null;
+            selector = (inputList) => {
+
+                inputList.ForEach(b =>
+                {
+                    var nameWords = b.Name.Split(splitChars).Where(n => !String.IsNullOrWhiteSpace(n));
+                    b.Likeness = (filters.Contains(b.Id) ? 1 : 0)
+                        + filters.Count(f => f.Contains(b.Name, StringComparison.InvariantCultureIgnoreCase))
+                        + filters.Count(f => nameWords.Any(nw => nw.Contains(f, StringComparison.InvariantCultureIgnoreCase)))
+                        + filters.Count(f => b.Name.Contains(f, StringComparison.InvariantCultureIgnoreCase))
+                        + filters.Count(f => f.Contains(b.Url ?? "", StringComparison.InvariantCultureIgnoreCase))
+                        + filters.Count(f => b.Url != null && b.Url.Contains(f, StringComparison.InvariantCultureIgnoreCase));
+                    if (b.Likeness > 0)
+                    {
+                        _cntMatches++;
+                    }
+                    b.Hidden = onlyShowMatches && b.Likeness <= 0 && b.Type != "folder";
+                });
+                foreach (var bm in inputList)
+                {
+                    if (bm.Children != null)
+                        bm.Children = selector.Invoke(bm.Children);
+                }
+                return inputList;
+            };
+
+            sort = selector.Invoke(unorderedBookmarks);
+
+            return sort;
+        }
+
+        private void tb_search_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (DateTime.Now < _nextUpdateAllowed)
+            {
+                if (_refreshQueueTask == null)
+                    _refreshQueueTask = Task.Delay(_nextUpdateAllowed - DateTime.Now).ContinueWith((t) => tb_search_TextChanged(null, null));
+                return;
+            }
+
+            Task.Run(() =>
+            {
+                string srchTxt = "";
+                Application.Current.Dispatcher.Invoke(new Action(() => {
+                    srchTxt = tb_search.Text;
+                }));
+                bookmarks = RankBookmarks(srchTxt, bookmarks);
+                bookmarks = SortBookmarks(bookmarks);
+                Application.Current.Dispatcher.Invoke(new Action(() => {
+                    tv_bookmarks.ItemsSource = bookmarks;
+                    tb_numMatches.Text = _cntMatches.ToString();
+                }));
+
+            });
+
+            //Func<List<Child>, List<Child>> setLikeness = null;
+            //setLikeness = (bs) =>
+            //{
+            //    foreach (var b in bs)
+            //    {
+            //        b.Likeness = filters.Select(f => FuzzySharp.Fuzz.Ratio(f, b.Name) + FuzzySharp.Fuzz.Ratio(f, b.Url)).Sum();
+            //        if (b.Children != null)
+            //        {
+            //            b.Children = setLikeness.Invoke(b.Children);
+            //        }
+            //    }
+            //    return bs;
+            //};
+
+            //bookmarks = setLikeness.Invoke(bookmarks);
+
+            _refreshQueueTask = null;
+            _nextUpdateAllowed = DateTime.Now.AddSeconds(1).AddMilliseconds(500);
+        }
+
+        private async void btnRefresh_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            await Task.Run(UpdateBookmarkList);
+            tv_bookmarks.ItemsSource = bookmarks;
+            tb_numMatches.Text = _cntMatches.ToString();
+        }
+
+        private void cbx_onlyShowMatches_Click(object sender, RoutedEventArgs e)
+        {
+            tb_search_TextChanged(null, null);
+        }
+    }
+}
